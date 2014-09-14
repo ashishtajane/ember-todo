932b7ac0c62869eda45fd262acfee625d6d3f0ab
Ashish Tajane <ashishtajane@gmail.com>
2014-09-14 19:59:16 +0530

    Toggle all todos

--- js/controllers/todos_controller.js
+++ js/controllers/todos_controller.js
@@ -41,5 +41,15 @@ Todos.TodosController = Ember.ArrayController.extend({
   inflection: function() {
     var remaining = this.get('remaining');
     return remaining === 1 ? 'item' : 'items';
-  }.property('remaining')
+  }.property('remaining'),
+
+  allAreDone: function(key, value) {
+    if (value === undefined) {
+      return !!this.get('length') && this.isEvery('isCompleted');
+    } else {
+      this.setEach('isCompleted', value);
+      this.invoke('save');
+      return value;
+    }
+  }.property('@each.isCompleted')
 });
